04b3da56263c4e7c8a83963c333d6029d71088f2
Lamz16 <andiku0755@gmail.com>
2025-11-08 21:24:10 +0700

    initial projects

---
+++ bootstrap/app.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Helpers\ApiResponse;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->render(function (\Illuminate\Validation\ValidationException $e, $request){
+           return ApiResponse::error(
+               422,
+               $e->errors()
+           );
+        });
+
+        $exceptions->render(function (Throwable $e, $request){
+            return ApiResponse::error(
+                method_exists($e, 'getStatusCode') ? $e->getStatusCode() : 500,
+                $e->getMessage(),
+            );
+        });
+        //
+    })->create();
