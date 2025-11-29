5a3199e24f0ef845aed9ff19f3bd57790caf8771
Andi Salam Syahputra <andiku0755@gmail.com>
2025-11-29 20:24:48 +0700

    add many routes

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,30 @@
 <?php
 
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\KatalogBatikController;
 use App\Http\Controllers\KursusMembatikController;
 use App\Http\Controllers\ProvinsiController;
 use App\Http\Controllers\RekomendasiController;
 use App\Http\Controllers\VideoBatikController;
 use App\Http\Controllers\WisataController;
+use App\Models\katalog_batik;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('provinsi', ProvinsiController::class);
 Route::apiResource('wisata', WisataController::class);
 Route::apiResource('video-batik', VideoBatikController::class);
+Route::apiResource('berita', BeritaController::class);
 
 Route::apiResource('rekomendasi', RekomendasiController::class);
 Route::post('rekomendasi/{id}', [RekomendasiController::class, 'updateRekomendasi']);
 
 Route::apiResource('kursus', KursusMembatikController::class);
 Route::post('kursus/{id}', [KursusMembatikController::class, 'updateKursus']);
+
+Route::apiResource('katalog-batik', KatalogBatikController::class);
+Route::post('katalog-batik/{id}', [KatalogBatikController::class, 'updateKatalog']);
+
+
+
 
