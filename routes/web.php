1cf28742f95d701e7466b5063cfeda4a00a00d64
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-10 22:06:06 +0700

    remove welcome.blade.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/admin/dashboard', function () {
     return view('admin.index');
 })->name('admin.dashboard');
