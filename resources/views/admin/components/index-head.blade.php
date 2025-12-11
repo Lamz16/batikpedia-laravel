4d1353ed128f6b717f9e71191f528b09ede8720c
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-11 08:44:26 +0700

    add index-head.blade.php

---
+++ resources/views/admin/components/index-head.blade.php
@@ -0,0 +1,12 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>PolluxUI Admin</title>
+    <!-- base:css -->
+    <link rel="stylesheet" href="{{asset('build/vendors/typicons/typicons.css')}}">
+    <link rel="stylesheet" href="{{asset('build/vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet" href="{{asset('build/css/vertical-layout-light/style.css')}}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{asset('build/images/favicon.png')}}" />
+</head>
