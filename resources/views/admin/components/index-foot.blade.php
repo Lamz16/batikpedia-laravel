90c144703fdf826649f9512dcff164c3c98a5126
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-11 08:44:14 +0700

    add index-head.blade.php

---
+++ resources/views/admin/components/index-foot.blade.php
@@ -0,0 +1,16 @@
+<!-- base:js -->
+<script src="{{asset('build/vendors/js/vendor.bundle.base.js')}}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page-->
+<script src="{{asset('build/vendors/chart.js/Chart.min.js')}}"></script>
+<!-- End plugin js for this page-->
+<!-- inject:js -->
+<script src="{{asset('build/js/off-canvas.js')}}"></script>
+<script src="{{asset('build/js/hoverable-collapse.js')}}"></script>
+<script src="{{asset('build/js/template.js')}}"></script>
+<script src="{{asset('build/js/settings.js')}}"></script>
+<script src="{{asset('build/js/todolist.js')}}"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="{{asset('build/js/dashboard.js')}}"></script>
+<!-- End custom js for this page-->
