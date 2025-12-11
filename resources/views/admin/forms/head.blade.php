e5878e0edd8187fe2320c06d6c43a55d7bf9284b
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-11 13:28:09 +0700

    update title

--- resources/views/admin/forms/head.blade.php
+++ resources/views/admin/forms/head.blade.php
@@ -2,7 +2,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>PolluxUI Admin</title>
+    <title>Batik Pedia Admin</title>
     <!-- base:css -->
     <link rel="stylesheet" href="{{asset('build/vendors/typicons/typicons.css')}}">
     <link rel="stylesheet" href="{{asset('build/vendors/css/vendor.bundle.base.css')}}">
@@ -14,5 +14,5 @@
     <!-- inject:css -->
     <link rel="stylesheet" href="{{asset('build/css/vertical-layout-light/style.css')}}">
     <!-- endinject -->
-    <link rel="shortcut icon" href="{{asset('build/images/favicon.png')}}" />
+    <link rel="shortcut icon" href="{{asset('build/images/tricakrawala.png')}}" />
 </head>
