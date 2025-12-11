acf1a1c869875a028d73956de56abb3deae3946b
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-11 08:44:38 +0700

    update index with include head and foot

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,22 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>PolluxUI Admin</title>
-  <!-- base:css -->
-  <link rel="stylesheet" href="{{asset('build/vendors/typicons/typicons.css')}}">
-  <link rel="stylesheet" href="{{asset('build/vendors/css/vendor.bundle.base.css')}}">
-  <!-- endinject -->
-  <!-- plugin css for this page -->
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="{{asset('build/css/vertical-layout-light/style.css')}}">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="{{asset('build/images/favicon.png')}}" />
-</head>
+@include('admin.components.index-head')
+
 <body>
   <div class="container-scroller">
     <!-- partial:partials/_navbar.html -->
@@ -94,22 +80,8 @@
   </div>
   <!-- container-scroller -->
 
-  <!-- base:js -->
-  <script src="{{asset('build/vendors/js/vendor.bundle.base.js')}}"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <script src="{{asset('build/vendors/chart.js/Chart.min.js')}}"></script>
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
-  <script src="{{asset('build/js/off-canvas.js')}}"></script>
-  <script src="{{asset('build/js/hoverable-collapse.js')}}"></script>
-  <script src="{{asset('build/js/template.js')}}"></script>
-  <script src="{{asset('build/js/settings.js')}}"></script>
-  <script src="{{asset('build/js/todolist.js')}}"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="{{asset('build/js/dashboard.js')}}"></script>
-  <!-- End custom js for this page-->
+  @include('admin.components.index-foot')
+
 </body>
 
 </html>
