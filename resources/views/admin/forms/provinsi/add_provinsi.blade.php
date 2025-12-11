6c9f87224324390495e94b35007ea2893929f3d8
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-11 11:48:39 +0700

    update provinsi add

--- resources/views/admin/forms/provinsi/add_provinsi.blade.php
+++ resources/views/admin/forms/provinsi/add_provinsi.blade.php
@@ -9,7 +9,7 @@
       @include('admin.components.topbar')
     <div class="container-fluid page-body-wrapper">
         @include('admin.components.theme-setting')
-      <!-- partial:../../partials/_sidebar.html -->
+        <!-- partial:../../partials/_sidebar.html -->
         @include('admin.components.navbar')
       <!-- partial -->
       <div class="main-panel">
@@ -18,46 +18,24 @@
             <div class="col-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
-                  <h4 class="card-title">Basic form elements</h4>
-                  <p class="card-description">
-                    Basic form elements
-                  </p>
+                  <h4 class="card-title">Tambah Data Provinsi</h4>
                   <form class="forms-sample">
                     <div class="form-group">
-                      <label for="exampleInputName1">Name</label>
-                      <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
+                      <label for="exampleInputName1">Nama Provinsi</label>
+                      <input type="text" class="form-control" id="exampleInputName1" placeholder="Nama Provinsi">
                     </div>
                     <div class="form-group">
-                      <label for="exampleInputEmail3">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputPassword4">Password</label>
-                      <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleSelectGender">Gender</label>
-                        <select class="form-control" id="exampleSelectGender">
-                          <option>Male</option>
-                          <option>Female</option>
-                        </select>
-                      </div>
-                    <div class="form-group">
-                      <label>File upload</label>
+                      <label>Upload Gambar Provinsi</label>
                       <input type="file" name="img[]" class="file-upload-default">
                       <div class="input-group col-xs-12">
-                        <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
+                        <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Gambar">
                         <span class="input-group-append">
                           <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
                         </span>
                       </div>
                     </div>
                     <div class="form-group">
-                      <label for="exampleInputCity1">City</label>
-                      <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleTextarea1">Textarea</label>
+                      <label for="exampleTextarea1">Detail Provinsi</label>
                       <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary mr-2">Submit</button>
