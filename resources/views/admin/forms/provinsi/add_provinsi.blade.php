f25f31d62ab47b59cf7019aa6a5def1051ba425b
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-12 09:35:21 +0700

    remove footer

--- resources/views/admin/forms/provinsi/add_provinsi.blade.php
+++ resources/views/admin/forms/provinsi/add_provinsi.blade.php
@@ -31,21 +31,6 @@
                         </div>
                     </div>
                 </div>
-                <!-- content-wrapper ends -->
-                <!-- partial:../../partials/_footer.html -->
-                <footer class="footer">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2020 <a
-                                        href="https://www.bootstrapdash.com/" class="text-muted" target="_blank">Bootstrapdash</a>. All rights reserved.</span>
-                                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center text-muted">Free <a
-                                        href="https://www.bootstrapdash.com/" class="text-muted" target="_blank">Bootstrap dashboard</a> templates from Bootstrapdash.com</span>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-                <!-- partial -->
             </div>
             <!-- main-panel ends -->
         </div>
@@ -54,6 +39,7 @@
 
     @include('admin.forms.foot')
     @include('admin.forms.provinsi.animation-alert-succes')
+    @include('admin.forms.provinsi.image-size-validation')
 
     <!-- End custom js for this page-->
     </body>
