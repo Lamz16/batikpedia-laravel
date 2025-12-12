7a4db8396edf07328b5f25357f90ed0d4cf17ae0
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-12 10:56:01 +0700

    add rekomendasi form

---
+++ resources/views/admin/forms/rekomendasi/add_rekomendasi.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('admin.forms.head')
+<body>
+<div class="container-scroller">
+    <!-- top bar-->
+    @include('admin.components.topbar')
+    <div class="container-fluid page-body-wrapper">
+        @include('admin.components.theme-setting')
+        <!-- partial:../../partials/_sidebar.html -->
+        @include('admin.components.navbar')
+        <!-- partial -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+                <div class="row">
+                    <div class="col-12 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Tambah Data Rekomendasi</h4>
+
+                                @if(session('success'))
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert"
+                                         id="successAlert">
+                                        {{ session('success') }}
+                                    </div>
+                                @endif
+
+                                @include('admin.forms.rekomendasi.forms-input')
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
+
+@include('admin.forms.foot')
+@include('admin.forms.partial.animation-alert-succes')
+@include('admin.forms.partial.image-size-validation')
+@include('admin.forms.rekomendasi.spinner')
+
+<!-- End custom js for this page-->
+</body>
+
+</html>
