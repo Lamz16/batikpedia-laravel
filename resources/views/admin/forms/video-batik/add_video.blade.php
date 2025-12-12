85f762342cda5008c3cd7fd0adf00999a47c518b
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-12 14:15:27 +0700

    add add_video.blade.php

---
+++ resources/views/admin/forms/video-batik/add_video.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+@include('admin.forms.head')
+<body>
+<div class="container-scroller">
+    <!-- top bar-->
+    @include('admin.components.topbar')
+    <div class="container-fluid page-body-wrapper">
+        @include('admin.components.theme-setting')
+        <!-- partial:../../partials/_sidebar.html -->
+        @include('admin.components.navbar')
+        <!-- partial -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+                <div class="row">
+                    <div class="col-12 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Tambah Data Video Batik</h4>
+
+                                @if(session('success'))
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert"
+                                         id="successAlert">
+                                        {{ session('success') }}
+                                    </div>
+                                @endif
+
+                                @include('admin.forms.video-batik.forms-video')
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+<!-- base:js -->
+@include('admin.forms.foot')
+@include('admin.forms.partial.animation-alert-succes')
+<!-- End custom js for this page-->
+</body>
+
+</html>
