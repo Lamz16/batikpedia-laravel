4d272c342c07218e6899e2655ef219e452961dd5
Andi Salam Syahputra <andiku0755@gmail.com>
2025-12-12 10:56:42 +0700

    optimize code

--- resources/views/admin/forms/provinsi/add_provinsi.blade.php
+++ resources/views/admin/forms/provinsi/add_provinsi.blade.php
@@ -1,47 +1,48 @@
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    @include('admin.forms.head')
-    <body>
-    <div class="container-scroller">
-        <!-- top bar-->
-        @include('admin.components.topbar')
-        <div class="container-fluid page-body-wrapper">
-            @include('admin.components.theme-setting')
-            <!-- partial:../../partials/_sidebar.html -->
-            @include('admin.components.navbar')
-            <!-- partial -->
-            <div class="main-panel">
-                <div class="content-wrapper">
-                    <div class="row">
-                        <div class="col-12 grid-margin stretch-card">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h4 class="card-title">Tambah Data Provinsi</h4>
+@include('admin.forms.head')
+<body>
+<div class="container-scroller">
+    <!-- top bar-->
+    @include('admin.components.topbar')
+    <div class="container-fluid page-body-wrapper">
+        @include('admin.components.theme-setting')
+        <!-- partial:../../partials/_sidebar.html -->
+        @include('admin.components.navbar')
+        <!-- partial -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+                <div class="row">
+                    <div class="col-12 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Tambah Data Provinsi</h4>
 
-                                    @if(session('success'))
-                                        <div class="alert alert-success alert-dismissible fade show" role="alert" id="successAlert">
-                                            {{ session('success') }}
-                                        </div>
-                                    @endif
+                                @if(session('success'))
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert"
+                                         id="successAlert">
+                                        {{ session('success') }}
+                                    </div>
+                                @endif
 
-                                  @include('admin.forms.provinsi.forms-input')
-                                </div>
+                                @include('admin.forms.provinsi.forms-input')
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- main-panel ends -->
         </div>
-        <!-- page-body-wrapper ends -->
+        <!-- main-panel ends -->
     </div>
+    <!-- page-body-wrapper ends -->
+</div>
 
-    @include('admin.forms.foot')
-    @include('admin.forms.provinsi.animation-alert-succes')
-    @include('admin.forms.provinsi.image-size-validation')
+@include('admin.forms.foot')
+@include('admin.forms.partial.animation-alert-succes')
+@include('admin.forms.partial.image-size-validation')
 
-    <!-- End custom js for this page-->
-    </body>
+<!-- End custom js for this page-->
+</body>
 
-    </html>
+</html>
